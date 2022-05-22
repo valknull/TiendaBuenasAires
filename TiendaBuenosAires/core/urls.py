d1868130c362414001764a320b82c9579ad9e71f
xnacho123 <82223668+xnacho123@users.noreply.github.com>
2022-05-22 11:34:29 -0400

    todo

--- TiendaBuenosAires/core/urls.py
+++ TiendaBuenosAires/core/urls.py
@@ -3,5 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('home',home,name='home'),
-    path('',login, name='login')
+    path('',login, name='login'),
+    path('itemP',itemP, name='item'),
+    path('perfil',perfil, name='perfil'),
+    path('sds',sds, name='sds'),
+    path('facturas',facturas, name='facturas')
 ]
--- TiendaBuenosAires/core/views.py
+++ TiendaBuenosAires/core/views.py
@@ -5,3 +5,21 @@ def home(request):
     return render(request,'home.html')
 def login(request):
     return render(request,'login.html')
+
+def itemP(request):
+    return render(request, 'item.html')
+
+def perfil(request):
+    usuario = {
+        "nombre": "John",
+        "apellido": "Doe"
+    }
+    context = {
+        'usuario':usuario
+    }
+    
+    return render(request,'perfil.html', context)
+def facturas(request):
+    return render(request, 'facturas.html')
+def sds(request):
+    return render(request,'sds.html')
